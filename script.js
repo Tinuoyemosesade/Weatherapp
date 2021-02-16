7bde16a9144d575f5d55f4b7cc6ceed61547144a
Moses Tinuoye <tinuoyemosesade@gmail.com>
2021-02-16 16:20:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,21 @@ let weather = {
        this.apiKey
     )
       .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then((data) =>this.displayWeather(data));
   },
-  displayWeather: function (data) {},
+  displayWeather: function (data) {
+    const { name } = data;
+    const { description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    console.log(name, icon, description, temp, humidity);
+    document.querySelector(".temp").innerText = temp + "°C";
+    document.querySelector(".location").innerText = "Weather in " + name;
+    document.querySelector(".weather"). innerText = description;
+  },
 };
+
+document.querySelector(".search-bar").addEventListener("keyup", function(event){
+  if (event.key == "Enter") {
+    weather.search();
+  }
+})
