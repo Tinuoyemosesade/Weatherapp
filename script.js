715cc6101b3b14088b98e720d13eed3aba0b31f1
Moses Tinuoye <tinuoyemosesade@gmail.com>
2021-02-17 08:32:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,24 +5,34 @@ let weather = {
       "http://api.openweathermap.org/data/2.5/weather?q=" +
         city +
         "&appid=" +
-       this.apiKey
+       this.apiKey + "&units=metric"
     )
       .then((response) => response.json())
       .then((data) =>this.displayWeather(data));
   },
   displayWeather: function (data) {
+    console.log(data)
     const { name } = data;
     const { description } = data.weather[0];
     const { temp, humidity } = data.main;
-    console.log(name, icon, description, temp, humidity);
+    console.log(name, description, temp, humidity);
     document.querySelector(".temp").innerText = temp + "°C";
     document.querySelector(".location").innerText = "Weather in " + name;
     document.querySelector(".weather"). innerText = description;
+    if (temp > 23){
+      document.querySelector(".app").classList.add("warm")
+    }else{
+      document.querySelector(".app").classList.remove("warm")
+
+    }
   },
 };
 
 document.querySelector(".search-bar").addEventListener("keyup", function(event){
+ // console.log(event)
   if (event.key == "Enter") {
-    weather.search();
+    const city = document.getElementById('search-bar').value;
+    weather.fetchWeather(city);
+    
   }
 })
