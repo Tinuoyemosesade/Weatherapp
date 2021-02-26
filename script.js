e3f1b02ba09b0b3a42ad828d02a33afce7b3089b
Moses Tinuoye <tinuoyemosesade@gmail.com>
2021-02-26 13:48:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let weather = {
   "apiKey": "fcb5f35e4d920642974c0cfbf9720f8b",
   fetchWeather: function (city) {
     fetch(
-      "http://api.openweathermap.org/data/2.5/weather?q=" +
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
         city +
         "&appid=" +
        this.apiKey + "&units=metric"
